253507bcb50858baac93cc15df50dbadf90a3624
amaanlari <amaanlari@outlook.com>
2024-03-23 03:10:08 +0530

    logging with spring boot

--- src/main/java/com/lari/todo/login/LoginController.java
+++ src/main/java/com/lari/todo/login/LoginController.java
@@ -1,16 +1,22 @@
 package com.lari.todo.login;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ch.qos.logback.classic.Logger;
+
 @Controller
 public class LoginController {
+
+    private Logger logger = (Logger) LoggerFactory.getLogger(LoginController.class);
     
     @RequestMapping("/login")
     public String login(@RequestParam String name, ModelMap model) {
         model.put("name", name);
+        logger.debug("The request parameter is: {}", name);
         return "login";
     }
 }
